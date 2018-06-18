e9781beeb9daeef9aedb359d36671794c1e4813f
Steve Ernstberger <sernstberger@developertown.com>
2018-06-18 14:09:04 -0400

    Added material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import './App.css';
 
+import Card from "@material-ui/core/Card";
+
 import logo from './logo.svg';
 
 class App extends React.Component {
@@ -9,11 +11,13 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Projekt</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <Card>
+          <p className="App-intro">
+            To get started, edit <code>src/App.tsx</code> and save to reload.
+          </p>
+        </Card>
       </div>
     );
   }
